0a23bdcba04dee9f58b91f56292b793199502dd3
dianrizkyrianti <85233866+dianrizkyrianti@users.noreply.github.com>
2021-07-04 18:15:40 +0700

    Add files via upload

---
+++ Git.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+/**
+ *
+ * @author asus
+ */
+public class Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+<<<<<<< HEAD
+        System.out.println("Ini branchPPBO2");
+=======
+        System.out.println("Ini branchPPBO");
+>>>>>>> branchPPBO
+    }
+    
+}
